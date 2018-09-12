e0e426bcef1ed77920a19adb46954200a8bbc750
Christian.Rowden <christian.rowden@ChristianRowdens-MacBook-Pro.local>
2018-09-12 10:43:32 -0500

    Started on Mac_Boilerplate.py

---
+++ Mac_Boilerplate.py
@@ -0,0 +1,7 @@
+# This program will use the MAC OS syntax wrapped in python to create a boilerplate that can be used to configure
+# any MAC to a suitable environment for my requirements
+
+import os 
+
+print("MAC_Boiler Initialized...")
+
